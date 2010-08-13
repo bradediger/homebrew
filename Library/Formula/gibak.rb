32fc40057b9ae24416be666b9d3d679331dcf17f
Brad Ediger <brad.ediger@madriska.com>
2010-08-13 14:46:24 -0500

    gibak: don't sync repos into .git; wastes space

--- Library/Formula/gibak.rb
+++ Library/Formula/gibak.rb
@@ -10,9 +10,31 @@ class Gibak < Formula
   depends_on 'objective-caml-findlib'
   depends_on 'objective-caml-fileutils'
 
+  def patches
+    DATA
+  end
+
   def install
     system "omake"
     # poor man's make install
     bin.install Dir['*']
   end
 end
+
+# [BE] Don't rsync git repos into .git... it rarely gains us anything, and
+# wastes a lot of space
+__END__
+diff --git a/gibak b/gibak
+index 7f63f41..a98209e 100755
+--- a/gibak
++++ b/gibak
+@@ -81,8 +81,6 @@ function __handle_git_repositories() {
+     mkdir -p "$base"
+     find-git-repos -i -z | while read -d $'\0' rep; do
+ 	echo "  submodule $rep" >&2
+-	rsync -a -F --relative --delete-excluded --delete-after \
+-	    "$rep" "$base"
+ 	printf '[submodule "%s"]\n\tpath = %s\n\turl= %s\n' \
+ 	    "$rep" "$rep" "$base/$rep/.git" >> .gitmodules
+     done
+

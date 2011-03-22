315e6c33dc6d968bb0617496feda34545e0ddc5a
Brad Ediger <brad.ediger@madriska.com>
2011-03-22 13:48:41 -0500

    Add updates to par2

--- Library/Formula/par2.rb
+++ Library/Formula/par2.rb
@@ -6,7 +6,9 @@ class Par2 <Formula
   sha1 '2fcdc932b5d7b4b1c68c4a4ca855ca913d464d2f'
 
   def patches
-    %w[http://sage.math.washington.edu/home/binegar/src/par2cmdline-0.4-gcc4.patch]
+    # gist: Debian's 003_fix_crash_in_quiet_mode.patch
+    %w[http://sage.math.washington.edu/home/binegar/src/par2cmdline-0.4-gcc4.patch
+    https://gist.github.com/raw/784811/83a1bb2d8283e269ae6e93355eaa68579f0272fd/gistfile1.diff]
   end
 
   def install
